0abc7fe23cc86199ce1b946a3db0a5a597c7429e
VanHoanTa2002 <109612816+hoantv2002@users.noreply.github.com>
2022-10-16 13:59:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Em';
+const nameGirl = 'Nga <3';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const titleCard = 'My best friend! 会いたい！';
+const contentCard = 'Nhân dịp 20/10 chúc Nga thật vui vẻ,xinh gái & tràn đầy năng lượng!😊Dẫu biết là cuộc sống 1 mình chưa bao jo là dễ dàng, nhưng tin Nga sẽ vượt qua thui!😊. Vậy hãy cố hơn nữa nhé!またね！';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
